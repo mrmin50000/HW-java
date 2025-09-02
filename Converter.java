7dd43a7f021ae051173a88ba8e89fdb70cb2acf3
mrmin50000 <danilast2007@gmail.com>
2025-09-02 09:57:38 +0300

    prak1.2

--- Converter.java
+++ Converter.java
@@ -11,6 +11,14 @@ public class Converter {
 
 		System.out.print("Yuan input: ");
 		yuan = input.nextInt();
+		if (yuan % 100 <= 20 && yuan % 100 >= 10)
+			System.out.println(String.format("Yuan output: %d юаней", yuan));
+		else if (yuan % 10 == 1)
+			System.out.println(String.format("Yuan output: %d юань", yuan));
+		else if (yuan % 10 >= 2 && yuan % 10 <= 4)
+			System.out.println(String.format("Yuan output: %d юаня", yuan));
+		else
+			System.out.println(String.format("Yuan output: %d юаней", yuan));
 
 		roubles = yuan * ROUBLES_PER_YUAN;
 		System.out.println(String.format("Roubles output: %f", roubles));
