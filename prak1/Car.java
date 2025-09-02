da178c1fbe6cc89785e26f341891e8f0d59778f4
mrmin50000 <danilast2007@gmail.com>
2025-09-02 10:45:40 +0300

    car.2

--- prak1/Car.java
+++ prak1/Car.java
@@ -8,7 +8,7 @@ public class Car {
 		model = "Sport Car";
 		license = "None";
 		color = "black";
-		year = "1985";
+		year = 1985;
 	}
 
 	public Car(String model, String license, String color, int year) {
@@ -22,4 +22,58 @@ public class Car {
 		this.model = model;
 		this.color = color;
 	}
+
+	public void To_String() {
+		System.out.println("Model: " + model);
+		System.out.println("License: " + license);
+		System.out.println("Color: " + color);
+		System.out.println("Year: " + year);
+	}
+
+	public String GetModel() {
+		return model;
+	}
+
+	public String GetLicense() {
+		return license;
+	}
+
+	public String GetColor() {
+		return color;
+	}
+
+	public int GetYear() {
+		return year;
+	}
+
+	public void SetModel(String model) {
+		this.model = model;
+	}
+
+	public void SetLicense(String license) {
+		this.license = license;
+	}
+
+	public void SetColor(String color) {
+		this.color = color;
+	}
+
+	public void SetYear(int year) {
+		this.year = year;
+	}
+
+	public int ReturnAge() {
+		return 2025 - this.year;
+	}
+
+	public static void main(String[] args) {
+		Car C = new Car();
+		C.To_String();
+		C.SetModel("merc");
+		System.out.print("\n");
+		C.To_String();
+		System.out.print("\n");
+		System.out.println("Age: " + C.ReturnAge());
+	}
+
 }
