53e4c51b05fbb9f270e4dd4faf4e3daab823b032
mrmin50000 <danilast2007@gmail.com>
2025-09-27 11:15:56 +0300

    task 1

--- prak5/Main1.java
+++ prak5/Main1.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import java.util.ArrayList;
 
-public class Circle implements Comparable {
-	double radius;
+class Circle implements Comparable<Circle> {
+	public double radius;
 
 	Circle(double radius) {
 		this.radius = radius;
@@ -16,10 +16,34 @@ public class Circle implements Comparable {
 	}
 
 	@Override
-	public int compareTo(Object arg0) {
-		if (this.radius > arg0.g) {
+	public int compareTo(Circle other) {
+		if (this.radius > other.radius) {
 			return 1;
+		} else if (this.radius < other.radius) {
+			return -1;
+		} else {
+			return 0;
 		}
+	}
+}
+
+class Array2DUtils {
+	public static <T extends Comparable<T>> T Max(T[][] array2D) {
+		if (array2D == null || array2D.length == 0) {
+			throw new IllegalArgumentException("array is null or length = 0");
+		}
+
+		T max = array2D[0][0];
+
+		for (int i = 0; i < array2D.length; i++) {
+			for (int j = 0; j < array2D[i].length; j++) {
+				if (array2D[i][j].compareTo(max) == 1) {
+					max = array2D[i][j];
+				}
+			}
+		}
+
+		return max;
 	}
 }
 
@@ -64,5 +88,22 @@ public class Main1 {
 		System.out.println("\n" + arrWithoutDuplicates);
 
 		System.out.println("\n" + search("apple", arr));
+
+		ArrayList<Circle> carr = new ArrayList<>();
+		carr.add(new Circle(12));
+		carr.add(new Circle(10));
+		carr.add(new Circle());
+		System.out.println("\n" + carr);
+		Circle max = new Circle(0);
+		for (int i = 0; i < carr.size() - 1; i++) {
+			if (carr.get(i).compareTo(max) == 1) {
+				max = carr.get(i);
+			}
+		}
+		System.out.println("\n" + max.radius + "\n");
+
+		Circle[][] arr2d = { { new Circle(12), new Circle(1) }, { new Circle(123), new Circle() },
+				{ new Circle(), new Circle() } };
+		System.out.println(Array2DUtils.Max(arr2d).radius);
 	}
 }
